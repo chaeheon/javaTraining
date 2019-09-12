230aedf8a62f02c8fca06bf408a459e8345bb073
user <user@DESKTOP-IU599UR>
2019-09-12 18:26:50 +0900

    백준 8393번 문제 첫 commit

---
+++ src/javaTraining/Bj_ex8393.java
@@ -0,0 +1,20 @@
+package javaTraining;
+import java.util.Scanner;
+//백준8393번 문제
+public class Bj_ex8393 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int sum=0;
+		int i;
+		while(i<=n) {
+			for(int i=1; i<=n; i++) {
+				sum+=i;
+			}
+
+		}
+		System.out.println(sum);
+		scanner.close();
+	}
+
+}
