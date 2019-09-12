5973a2a097e08622d36a683b1ccd183f96d58020
user <user@DESKTOP-IU599UR>
2019-09-12 17:35:51 +0900

    백준 1330번 문제 첫 commit

---
+++ src/javaTraining/BJ_ex1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+package javaTraining;
+//백준 1330번 문제
+public class BJ_ex1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+
+	}
+
+}
